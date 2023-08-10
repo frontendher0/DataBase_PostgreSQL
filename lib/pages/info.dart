793f5800d04bed8a3b6cbdc7600e45136eebb404
frontendher0 <her0.ru@yandex.ru>
2023-08-10 15:16:38 +0300

    mm

--- lib/pages/info.dart
+++ lib/pages/info.dart
@@ -2,57 +2,83 @@ import 'package:flutter/material.dart';
 import 'package:postgres/postgres.dart';
 
 class Info extends StatelessWidget {
-const Info({ Key? key }) : super(key: key);
+  const Info({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-     Widget logo(){
-    return Padding(
-    padding: const EdgeInsets.only(top: 80),
-    child: Container(
-      child: const Align(
-        child: Text('Информация об объекте:',
-        style: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
-         )
-        )
-       ) 
-      )
+  Widget build(BuildContext context) {
+    Widget logo() {
+      return Padding(
+          padding: const EdgeInsets.only(top: 80),
+          child: Container(
+              child: const Align(
+                  child: Text('Информация об объекте:',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      )))));
+    }
+
+    Widget information() {
+      return Container(
+        child: Column(children: [
+          Padding(
+            padding: EdgeInsets.only(top:20),),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            Expanded(
+                flex: 1,
+                child: Text('ID Объекта:', textAlign: TextAlign.justify ,  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)))
+          ]),
+          Padding(
+            padding: EdgeInsets.only(top:45),),
+           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            Expanded(
+                flex: 1,
+                child: Text('Имя проекта:', textAlign: TextAlign.justify ,  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)))
+          ]),
+          Padding(
+            padding: EdgeInsets.only(top:45),),
+           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            Expanded(
+                flex: 1,
+                child: Text('Описание проекта:', textAlign: TextAlign.justify ,  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)))
+          ]),
+          Padding(
+            padding: EdgeInsets.only(top:45),),
+           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            Expanded(
+                flex: 1,
+                child: Text('Номер телефона:', textAlign: TextAlign.justify ,  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)))
+          ]),
+          Padding(
+            padding: EdgeInsets.only(top:45),),
+           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            Expanded(
+                flex: 1,
+                child: Text('Фотография:', textAlign: TextAlign.justify ,  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)))
+          ])
+          
+        ]),
+      );
+    }
+    return Scaffold(
+    body: Column(children: [logo(),information()],)
     );
+  
   }
 
-
-Widget information(){
-  return Padding(
-    padding: EdgeInsets.only (top: 20, left: 20),
-    child: Container(
-      child: const Align(
-      child: Text('ID: $Jopa.name ', )
-
-      )
-
+  
 }
 
- return Scaffold(
-    body: Column(children: [
-      logo()],
-      )
-      return Card()
-  );
- 
 
-    
-  }
-}
 
-class Object{
-  int id;
-  String name;
-  String description;
-  //Byte picture[];
-  Object({ required this.id, required this.name, required this.description, /*required this.picture*/});
-}
-
-Object Jopa = Object(id: 228, name : 'jopa', description: 'idite');
